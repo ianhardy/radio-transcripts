229c6c45a5b130350acdbf96ffb3998b79884cf8
ianhardy <ian.c.hardy@gmail.com>
2013-06-22 15:05:06 -0400

    fixed encoding errors, added trans w/metadata

--- rehmscraper.py
+++ rehmscraper.py
@@ -4,14 +4,18 @@
 from bs4 import BeautifulSoup as bs
 import urllib2
 import datetime
-import nltk
 import time
+import os
 
 PAUSE = 10
 ONE_DAY = datetime.timedelta(days=1)
 URL_STEM = 'http://thedianerehmshow.org'
-START_DATE = datetime.date(2013, 06, 01)
+START_DATE = datetime.date(2013, 01, 01)
 END_DATE = datetime.date.today()
+
+# make directories to collect files in
+os.makedirs('corpus')
+os.makedirs('trans_collection')
 
 # Collect the following: 
 description_urls = []
@@ -39,14 +43,19 @@ def scrapetransurls():
     except:
       pass
 
-# grab the transcript content, spoken only--not metadata, and write it to a text file
+# grab the transcript content and write it to a clean file, also save a copy with metadata
 def scrapetranscripts():
   for url in transcript_urls:
     transpage = bs(urllib2.urlopen(url))
     trans_content = transpage.findAll('div', {'class' : 'trans-event-content'})
-    f = open((url[34:44] + url[45:50] +  '_rehm.txt'), 'w') 
-    f.write(nltk.clean_html(str(trans_content)))
-    f.close()
+    transfile = open('./corpus/' + url[34:44] + url[45:50] +  '_rehm.txt', 'w') 
+    for speech in trans_content:
+      transfile.write(speech.text.encode('utf-8'))
+    transfile.close()
+    all_content = transpage.find('div', {'id' : 'content'})
+    trans_plusmeta = open('./trans_collection/' + url[34:44] + url[45:50] + '_rehm.txt', 'w')
+    trans_plusmeta.write(all_content.text.encode('utf-8'))
+    trans_plusmeta.close()
     time.sleep(PAUSE)
 
 makeurls()
